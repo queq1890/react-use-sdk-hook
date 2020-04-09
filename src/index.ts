4a3f6515ca7469a2f553c1f1fc0608c3ba38653d
Yuji Matusmoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-04-09 20:35:23 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect, useCallback } from 'react';
+
+type Options = {
+  src: string;
+  cleanUpFlag?: boolean;
+};
+
+const useSDK = ({ src, cleanUpFlag = false }: Options) => {
+  const [loaded, setLoaded] = useState(false);
+  const cleanUp = useCallback(() => {
+    const script = document.querySelector(`script[src=${src}]`);
+    if (script) document.removeChild(script);
+  }, [src]);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = src;
+    document.body.appendChild(script);
+    setLoaded(true);
+
+    return () => {
+      if (cleanUpFlag) {
+        cleanUp();
+      }
+    };
+  }, [src, cleanUpFlag, cleanUp]);
+
+  return [loaded, cleanUp];
+};
+
+export default useSDK;
